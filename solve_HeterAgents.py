54d0d3f7cae884bc128790b77d67668788232792
Minjie Deng <mdeng7@ur.rochester.edu>
2019-02-03 11:44:47 -0500

    Add files via upload

---
+++ solve_HeterAgents.py
@@ -0,0 +1,5 @@
+# Computational Method
+# developed by Thomas Winberry (2018), "A Method for Solving and Estimating Heterogeneous Agent Macro Models"
+# Step 1: Approximate value function & distribution using finite-dimensional global approximations w.r.t. individual state variables
+# Step 2: Compute the stationary eqm. of the finite-dimensional model (no agg. shocks, have idiosyncratic shocks)
+# Step 3: Compute the agg. dynamics of th finite-dimensional model using Taylor expansion around the stationary eqm.
